6b16eaf316c9a3327f845db1b693aba8fa21a89d
Grunter <jongranberg@hotmail.se>
2017-08-16 15:55:43 +0200

    First commit to snake game

--- snakeGame/src/snakeGame.cpp
+++ snakeGame/src/snakeGame.cpp
@@ -7,9 +7,12 @@
 //============================================================================
 
 #include <iostream>
+#include <SDL.h>
+#undef main
 using namespace std;
 
 int main() {
+	SDL_Init(SDL_INIT_VIDEO);
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
